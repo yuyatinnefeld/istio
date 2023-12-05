481f391d3fd6b1e687b6b21cceecd973b2ce857b
yuyatinnefeld <yuyawamo@gmail.com>
2023-12-05 08:05:46 +0100

    feat: add java application

---
+++ microservices/app/details/Details.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Details {
+    public static void main(String[] args) throws Exception {
+        final int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        System.out.println("Serving on port 8080..\n");
+        server.start();
+    } 
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            // Logging IP address of request to stdout
+            System.out.println("Request received from: " + t.getRemoteAddress().toString());
+
+            // Displaying json string
+            String jsonObj = "{app:details-app, version: 1.0.0, language: java}";
+
+            String response = "<html><body><p>" + jsonObj + "</p>\n";
+            response += "</ul></body></html>\n";
+
+            // Sending response
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
