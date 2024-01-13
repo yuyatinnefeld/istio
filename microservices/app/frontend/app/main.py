e19cdc227ab0b425ecc3c085f7d93e72069f4a8f
yuyatinnefeld <yuyawamo@gmail.com>
2024-01-13 12:54:35 +0100

    feat: update frontend app with login feature

--- microservices/app/frontend/app/main.py
+++ microservices/app/frontend/app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+
 import json
 import requests
 
@@ -42,6 +43,33 @@ def test():
     return jsonify(message)
 
 
+@app.route('/login')
+def login():
+    user_name = request.headers.get('end-user', 'Unknown User')
+    return render_template('index.html', user_name=user_name)
+
+
+@app.route('/set_end_user', methods=['POST'])
+def set_end_user():
+    user_name = request.form.get('user_name')
+
+    # CHECK HERE WITCH MODE YOU WANT TO HAVE!
+    is_api_from = "k8s"
+    frontend_version = "1.0.0"
+    
+    try:
+        if is_api_from == "k8s":
+            reviews_data = fetch_data('http://reviews-service:9999')
+        else:
+            reviews_data = "local reviews data"
+
+        message = f"USER: {user_name} | FRONTEND API: {frontend_version} - REVIEWS API: {reviews_data}"
+        return jsonify(message)
+
+    except Exception as exc:
+        print(exc)
+        return jsonify({'error': 'Failed to fetch data from backend APIs', 'message': exc}), 5000
+
 @app.route('/', methods=['GET'])
 def index():
     # CHECK HERE WITCH MODE YOU WANT TO HAVE!
@@ -64,9 +92,9 @@ def index():
         else:
             reviews_data = "dummy reviews data"
             payment_data = "dummy payment data"
-            details_data = fetch_data('http://localhost:7777') 
+            details_data = "dummy details data"
 
-        message = f"Frontend APP Version: {frontend_version} - Reviews API: {reviews_data}, Payment API: {payment_data}, Details API: {details_data}"
+        message = f"FRONTEND API: {frontend_version} - REVIEWS API: {reviews_data}, PAYMENT API: {payment_data}, DETAILS API: {details_data}"
         return jsonify(message)
 
     except Exception as exc:
