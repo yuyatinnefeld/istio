8e3fa449ebec189046d12e524f21614a6b9c34d4
yuyatinnefeld <yuyawamo@gmail.com>
2024-01-15 05:46:29 +0100

    fix: add error handling for limit requests in reviews app

--- microservices/app/reviews/main.go
+++ microservices/app/reviews/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+
+	"golang.org/x/time/rate"
 )
 
+var limiter = rate.NewLimiter(rate.Limit(5), 1) // Adjust the rate limit as needed
+var mu sync.Mutex
+
 func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("Server started")
-    log.Fatal(http.ListenAndServe(":9999", nil))
+	port := 9999
+	http.HandleFunc("/", MyHandler)
+	fmt.Printf("Serving on port %d..\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write([]byte(`{"app":"reviews", "version": "1.0.0", "language": "golang"}`))
+func MyHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	// Rate limiting
+	if !limiter.Allow() {
+		http.Error(w, "Too many requests. Please try again later.", http.StatusTooManyRequests)
+		return
+	}
+
+	// Logging IP address of request to stdout
+	fmt.Println("Request received from:", r.RemoteAddr)
+
+	// Displaying JSON string
+	response := `{"app": "reviews", "version": "1.0.0", "language": "golang"}`
+
+	// Sending response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, response)
 }
